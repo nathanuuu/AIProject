86696f77cd0d45f19c3f378fcbfdeb1d7fab6f33
Nathan Wu <mail.nathan.wu@gmail.com>
2015-12-06 19:07:34 -0500

    Edit the theta to be cross product of two features

--- Crime.py
+++ Crime.py
@@ -5,7 +5,7 @@ class Crime(object):
     # A method that returns between 0 and 1 how bad a crime is
     @staticmethod
     def severeness(desc):
-        crimeClass = [("theft", 1.0)]
+        crimeClass = [("assault", 1.0)]
                      # [("burglary", 1.0), ("theft", 1.0), ("robbery", 1.0),
                      #  ("dui", 1.0), ("assault", 1.0), ("arson", 1.0), 
                      #  ("kidnapping", 1.0)]
