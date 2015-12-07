54c845bc7450c954591fcaf2919996ae5adb723e
Nathan Wu <mail.nathan.wu@gmail.com>
2015-12-07 01:40:47 -0500

    Arrest finished.

--- Arrest.py
+++ Arrest.py
@@ -1,10 +1,10 @@
 # This arrest class is an extension to the ml class
-# Given an arrest, and a neighborhood, what is the likely
-# age and gender of the perp.
+# Given the neighborhood, age, gender, predict whether it's type of crime
 
 from ML import ML
 from TableOp import TableOp
 import numpy as np
+from Crime import Crime
 
 class Arrest(ML):
     
@@ -14,19 +14,109 @@ class Arrest(ML):
             ["REPORT_NAME"], ["ARREST"])
         # and take the stuff we need
         self.dataArray = TableOp.reduceColumns(self.dataArray, 
-            ["DESCRIPTION", "ARREST_TIME", "NEIGHBORHOOD", "AGE", "GENDER"])
+            ["DESCRIPTION", "NEIGHBORHOOD", "AGE", "GENDER"])
         # we give each neighborhood a numerical representation
         (self.dataArray, self.nidDict) = TableOp.numerify(
             self.dataArray, "NEIGHBORHOOD")
         self.neighborhoodCount = len(self.nidDict) # number of neighborhoods
-        print "Alert: in Arrest, time is 3 hours per interval"
-        self.timeIntervals = 60 * 3 # in minutes
-        self.timeIntervalCount = 60 * 24 / self.timeIntervals
+        self.ageGroupCount = 6
+        self.genderCount = 2
+        print len(self.dataArray)
 
 
     def makeX(self):
-        pass
+        self.XTable = TableOp.reduceColumns(self.dataArray,
+            ["NEIGHBORHOOD", "AGE", "GENDER"])
+        self.XTable = TableOp.dropEntries(self.XTable, 
+            ["AGE", "GENDER"], ["", ""])
+        self.updateAge()
+        self.updateGender()
+        (self.Xrows, self.Xcols) = (len(self.XTable) - 1, 
+            self.neighborhoodCount * self.ageGroupCount * self.genderCount)
+        self.X = [[0.0 for i in xrange(self.Xcols)] for j in xrange(self.Xrows)]
+        for i in xrange(len(self.X)):
+            [n, a, g] = self.XTable[i + 1] # skip header
+            index = (n * self.ageGroupCount * self.genderCount
+                + a * self.genderCount + g)
+            self.X[i][index] = 1.0
+        self.X = np.array(self.X)
+
+
+    def updateAge(self):
+        for i in xrange(1, len(self.XTable), 1):
+            age = int(self.XTable[i][1])
+            if (0 < age < 15):
+                self.XTable[i][1] = 0
+            elif (15 <= age < 18):
+                self.XTable[i][1] = 1
+            elif (18 <= age < 21):
+                self.XTable[i][1] = 2
+            elif (21 <= age < 45):
+                self.XTable[i][1] = 3
+            elif (45 <= age < 65):
+                self.XTable[i][1] = 4
+            else:
+                self.XTable[i][1] = 5
+
+
+    def updateGender(self):
+        for i in xrange(1, len(self.XTable), 1):
+            gender = self.XTable[i][2]
+            if (gender == "M"):
+                self.XTable[i][2] = 0
+            else:
+                self.XTable[i][2] = 1
 
 
     def makeY(self):
-        pass
+        self.YTable = TableOp.reduceColumns(self.dataArray, ["DESCRIPTION"])
+        crimeClass = [("burglary", 1.0)]
+        (self.Yrows) = self.Xrows
+        self.Y = [0.0 for i in xrange(self.Yrows)]
+        print self.YTable
+        for i in xrange(len(self.Y)):
+            desc = self.YTable[i + 1][0] # skip header
+            self.Y[i] = Crime.severeness(desc, crimeClass)
+        self.Y = np.array(self.Y)
+
+
+    def printTheta(self):
+        self.printableTheta = self.theta.tolist()
+        for i in xrange(len(self.printableTheta)):
+            self.printableTheta[i] = (self.recoverLabels(i), 
+                self.printableTheta[i])
+        self.printableTheta = sorted(self.printableTheta, 
+            key = lambda x: x[1], reverse = True)
+        for i in xrange(len(self.printableTheta)):
+            print self.printableTheta[i]
+
+
+    def recoverLabels(self, index):
+        # gender
+        if (index % 2 == 0): 
+            sString = "M "
+        else: 
+            sString = "F "
+        # age group
+        a = (index % (self.ageGroupCount * self.genderCount)) / self.genderCount
+        if (a == 0):
+            aString = "under 15 "
+        elif (a == 1):
+            aString = "15 to 17 "
+        elif (a == 2):
+            aString = "18 to 20 "
+        elif (a == 3):
+            aString = "21 to 44 "
+        elif (a == 4):
+            aString = "45 to 64 "
+        else:
+            aString = "65 and above "
+        # neighborhood
+        n = index / (self.genderCount * self.ageGroupCount)
+        nString = self.nidDict[n]
+        return sString + aString + nString
+
+
+
+
+
--- main.py
+++ main.py
@@ -8,15 +8,17 @@ class BlotterML(object):
     # Initialize the blotter problem
     def __init__(self, url):
         self.url = url
-        self.dataArray = rd.fromURL(self.url)
-        # print "Alert: using dummy input"
-        # self.dataArray = rd.fromFile("dummyInput.txt")
+        # self.dataArray = rd.fromURL(self.url)
+        print "Alert: using dummy input"
+        self.dataArray = rd.fromFile("dummyInput2.txt")
 
     # Runs the machine learning problem on arrest records
     def runArrest(self):
         aML = Arrest(self.dataArray)
         aML.makeDataSets()
         aML.makeVectors()
+        aML.learningAlgo()
+        aML.printTheta()
 
     # Runs the machine learning problem on offense records
     def runOffense(self):
@@ -30,8 +32,8 @@ class BlotterML(object):
 u = "https://data.wprdc.org/datastore/dump/c0fcc09a-7ddc-4f79-a4c1-9542301ef9dd"
 B = BlotterML(u)
 # Run it
-B.runOffense()
-#B.runArrest()
+#B.runOffense()
+B.runArrest()
 # we can generate output by calling the lineOutput function of the problem
 
 
